75a34dea0b6994cede1528c24d758fafb8d6b40d
liu <fddiljf@126.com>
2022-05-07 16:23:13 +0800

    remote

--- src/page/RemoteIframe.jsx
+++ src/page/RemoteIframe.jsx
@@ -1,28 +1,15 @@
 import React, { Fragment } from 'react'
 import Redirect404 from './Redirect404'
-import { post } from '../config/client'
-import StringUtils from '../util/StringUtils'
-import { useRequest } from 'ahooks';
-async function queryData(item) {
-     let v = item && item.value;
-     if (!StringUtils.isEmpty(v) && v.indexOf('http') === 0) {
-          item.publishUri = v;
-          return new Promise((resolve) => {
-               resolve(item);
-          });
-     }
-     return post("hot/info", {
-          hotKey: v
-     }).then((result) => result.resultData)
-}
 
 export default function RemoteIframe(props) {
-     const { data } = useRequest(() => queryData(props.item), { loadingDelay: 1000 })
      return (
           <Fragment>
-               {(data && data.publishUri) ? (<iframe
-                    src={data.publishUri} title={data.title}
-                    style={{ border: 'none', height: 'calc(100vh - 110px)', width: '100%' }}></iframe>) : (<Redirect404 />)}
+               {(props.item && props.item.value)
+                    ? (<iframe
+                         src={props.item.value} title={props.item.title}
+                         style={{ border: 'none', height: 'calc(100vh - 110px)', width: '100%' }}></iframe>)
+                    : (<Redirect404 />)
+               }
           </Fragment>
      );
 }
--- src/page/RemoteMirco.jsx
+++ src/page/RemoteMirco.jsx
@@ -1,34 +1,15 @@
-import React from 'react'
-import { post } from '../config/client'
-import StringUtils from '../util/StringUtils'
-import { useRequest } from 'ahooks';
+import React, { useEffect } from 'react'
 import { loadMicroApp } from 'qiankun';
 import Hold from './Hold'
 
-async function queryData(item) {
-     let v = item && item.value;
-     if (!StringUtils.isEmpty(v) && v.indexOf('http') === 0) {
-          item.publishUri = v;
-          return new Promise((resolve) => {
-               resolve(item);
-          });
-     }
-     return post("hot/info", {
-          hotKey: v
-     }).then((result) => result.resultData)
-}
-
 export default function RemoteMirco(props) {
-     useRequest(() => queryData(props.item), {
-          loadingDelay: 1000,
-          onSuccess: (data) => {
-               loadMicroApp({
-                    name: data.title,
-                    entry: data.publishUri,
-                    container: `#container-${props.item.key}`
-               });
-          }
-     })
+     useEffect(() => {
+          props.item && loadMicroApp({
+               name: props.item.title,
+               entry: props.item.value,
+               container: `#container-${props.item.key}`
+          });
+     }, [])
      return (
           <div style={{ border: 'none', height: 'calc(100vh - 110px)', width: '100%' }}
                id={`container-${props.item.key}`}>
